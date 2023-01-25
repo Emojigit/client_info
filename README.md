420ad191b38575234550bf16ef6627cb7dc9ceb7
Emoji <55009343+Emojigit@users.noreply.github.com>
2023-01-25 08:11:02 +0800

    new init!

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# my_info
+# The in-depth information of a player...
+This mod simply fetch the data from `minetest.get_player_information`. Any players can access their own information by executing `/user_info` with no parameters, and admins can access any's by adding their name as the paramater.
--- init.lua
+++ init.lua
@@ -1,18 +1,27 @@
+local S = minetest.get_translator("client_info")
+
 minetest.register_chatcommand("user_info",{
-	param = "<user>",
-	description = "Get your client's info",  -- Full description
+	param = S("[<user>]"),
+	description = S("Get the information of youself, or any player if you are admin."),
 	func = function(name, param)
-		if not(param == "") then
-			local havepriv, m = minetest.check_player_privs(name, {server=true})
+		if param and param ~= "" then
+			local havepriv, missing_privs = minetest.check_player_privs(name, {server=true})
 			if not(havepriv) then
-				return false, "No privs to do this!"
+				return false, minetest.translate("__builtin","You don't have permission to run this command "
+				.. "(missing privileges: @1).",
+				table.concat(missing_privs, ", "))
 			end
 			name = param
 		end
 		local info = minetest.get_player_information(name)
-		if not info then return false, "player not found!" end
-		minetest.chat_send_player(name, "Info of "..name..":")
-		minetest.chat_send_player(name, "IP: "..info.address.." (IPv"..tostring(info.ip_version)..")")
-		minetest.chat_send_player(name, "Uptime: "..tostring(info.connection_uptime).."sec")
+		if not info then return false, S("Player does not exist, or is not online.") end
+		return true,S("Information of @1",name) .. "\n" ..
+					S("IP: @1 (IPv@2)",info.address,info.ip_version) .. "\n" ..
+					S("Uptime: @1 seconds",info.connection_uptime) .. "\n" ..
+					S("Protocol Version: @1",info.protocol_version) .. "\n" ..
+					S("Formspec Version: @1",info.formspec_version) .. "\n" ..
+					S("Language: @1",info.lang_code) .. "\n" ..
+					S("RTT: @1~@2 (avg. @3)",info.min_rtt,info.max_rtt,info.avg_rtt) .. "\n" ..
+					S("Package time jitter: @1~@2 (avg. @3)",info.min_jitter,info.max_jitter,info.avg_jitter)
 	end,
 })
